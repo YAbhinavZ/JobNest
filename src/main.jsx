b58489dc71afa8fbba5044e78b5c904d46c59444
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-06-12 12:34:12 +0530

    clerk and supabase connection

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
 import { ClerkProvider } from '@clerk/react'
 import { shadesOfPurple } from '@clerk/themes'
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <ClerkProvider appearance={{
     baseTheme: shadesOfPurple
-  }}>
+  }} publishableKey={PUBLISHABLE_KEY}>
   <App />
   </ClerkProvider>
  
